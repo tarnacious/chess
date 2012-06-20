f3ff80d5b7c58f740d099b0659bd6e5c90dfe79c
tarn <tarn.barford@gmail.com>
2012-06-21 06:42:18 +1000

    Get all possible moves for a state

--- Play.hs
+++ Play.hs
@@ -7,6 +7,8 @@ import Game
 
 -- :l Play 
 -- putStr $ pretty $ tryplay $ tryplay initial
+--
+-- getMoves initial
 
 pretty                 :: Maybe State -> String 
 pretty Nothing         = "No board :("
@@ -14,13 +16,26 @@ pretty (Just a)        = case a of
                             (White, c) -> "White \n\n" ++ (prettyBoard c)
                             (Black, c) -> "Black \n\n" ++ (prettyBoard c)
                             
+
 initial                :: Maybe State
 initial                = case (boardParser $ prettyBoard $ initialBoard) of
                             (Just b) -> Just (White, b)
                             otherwise -> Nothing 
 
+
 tryplay                :: Maybe State -> Maybe State
 tryplay Nothing        = Nothing
 tryplay (Just b)       = Just (doMove b) 
 
+
+getMoves               :: Maybe State -> [(PieceType, (Int, Int), (Int, Int))]
+getMoves (Just(c, b))  = concat $ map (\m -> getPieceMoves b m) (colorPos c b) 
+getMoves _             = []
+
+
+getPieceMoves          :: Board -> (Int, Int) -> [(PieceType, (Int, Int), (Int, Int))]
+getPieceMoves b p      = case getSquare b p of
+                            (Just (Piece t c)) -> case genPieceMoves b p (Piece t c) of
+                                           pos -> map (\to -> (t,p,to)) pos
+                            otherwise -> []
 
